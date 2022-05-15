d3acf1ddf1b889b61c9c07e07f2436d3218c2f01
Francisco Guerrero <90232206+FranciscoGuerrero58@users.noreply.github.com>
2022-05-15 18:28:16 -0500

    Update Hello_world.cu

--- Hello_world.cu
+++ Hello_world.cu
@@ -4,10 +4,11 @@ __global__ void print_from_gpu(void) {
 printf("Hello World! from thread [%d,%d] \
 From device\n", threadIdx.x,blockIdx.x);
 }
-Chapter 1
 int main(void) {
 printf("Hello World from host!\n");
 print_from_gpu<<<1,1>>>();
 cudaDeviceSynchronize();
 return 0;
 }
+
+//ejemplo tomado del libro "learn CUDA PROGRAMING"
