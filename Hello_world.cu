5fb8b69b179b826dba58ebc18f60668268a1cd6b
Francisco Guerrero <90232206+FranciscoGuerrero58@users.noreply.github.com>
2022-05-15 18:24:26 -0500

    Funcional

--- Hello_world.cu
+++ Hello_world.cu
@@ -4,3 +4,10 @@ __global__ void print_from_gpu(void) {
 printf("Hello World! from thread [%d,%d] \
 From device\n", threadIdx.x,blockIdx.x);
 }
+Chapter 1
+int main(void) {
+printf("Hello World from host!\n");
+print_from_gpu<<<1,1>>>();
+cudaDeviceSynchronize();
+return 0;
+}
